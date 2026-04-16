20bb09298c34feef3d2064722c3c4377a025453f
TotallyDIO <hunter.tadlock911@gmail.com>
2026-04-16 13:33:31 -0500

    the lion has a genius idea that will def work

--- etc/calamares/modules/btrfs.sh
+++ etc/calamares/modules/btrfs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
-if command -v disk -l | grep "sda" != "" &>/dev/null; then
+if command -v fdisk -l | grep "sda" != "" &>/dev/null; then
 
     #if [sudo btrfs check --readonly --force /dev/nvme0n1p2 | grep "WARNING" ]
-    echo "123"
-else
-    echo "456"
+    echo "SDA disk"
+elif command -v fdisk -l | grep "nvme" != "" &>/dev/null; then
+    echo "NVME disk"
+elif command -v fdisk -l | grep "sr" != "" &>/dev/null; then
+    echo "SR Drive"
 fi
