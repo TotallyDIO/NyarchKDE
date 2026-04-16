51487f9cd9d33ee9f2cf58d558ba1feb3355287a
TotallyDIO <hunter.tadlock911@gmail.com>
2026-04-16 14:12:28 -0500

    the lion has reached a roadblock

--- etc/calamares/modules/btrfs.sh
+++ etc/calamares/modules/btrfs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+NVME=F
 
-if command -v fdisk -l | grep "sda" != "" &>/dev/null; then
 
-    #if [sudo btrfs check --readonly --force /dev/nvme0n1p2 | grep "WARNING" ]
-    echo "SDA disk"
-elif command -v fdisk -l | grep "nvme" != "" &>/dev/null; then
-    echo "NVME disk"
-elif command -v fdisk -l | grep "sr" != "" &>/dev/null; then
-    echo "SR Drive"
+# Checks for partitions
+if command -v sudo fdisk -l | grep "nvme" == "" &>/dev/null; then
+    echo "123"
 fi
+
+
+
+
+echo "end"
