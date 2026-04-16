f126d677af2fcaa9ab11a1e54dc2e3cf31a918d1
TotallyDIO <hunter.tadlock911@gmail.com>
2026-04-16 13:27:27 -0500

    the lion is getting smarter

--- etc/calamares/modules/btrfs.sh
+++ etc/calamares/modules/btrfs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-if (disk -l | grep "sda") != ""; then
+if command -v disk -l | grep "sda" != "" &>/dev/null; then
 
     #if [sudo btrfs check --readonly --force /dev/nvme0n1p2 | grep "WARNING" ]
     echo "123"
+else
+    echo "456"
 fi
