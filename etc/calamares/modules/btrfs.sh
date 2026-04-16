1a60271e9dec3db2d297e9b29bf82b61e53b312b
TotallyDIO <hunter.tadlock911@gmail.com>
2026-04-15 23:03:05 -0500

    the lion also refuses to use proper commit messages till he makes progress

--- etc/calamares/modules/btrfs.sh
+++ etc/calamares/modules/btrfs.sh
@@ -1,5 +1,8 @@
-if [sudo fdisk -l | grep "nvme" != ""]; then
+#!/bin/bash
+
+
+if (disk -l | grep "sda") != ""; then
+
     #if [sudo btrfs check --readonly --force /dev/nvme0n1p2 | grep "WARNING" ]
     echo "123"
-    break
-echo "done"
+fi
